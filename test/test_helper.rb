284b9435f23e9b6ce36147f9776df50ff3b1717c
semdinsp <scott.sproule@ficonab.com>
2013-09-28 22:46:28 +0800

    another change to get clode climate working

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 require 'stringio'
 require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/randprize'
+require 'rubygems'
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
