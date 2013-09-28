ca0f18255a52a7e705f153ae0225eafceed0cb57
semdinsp <scott.sproule@ficonab.com>
2013-09-28 23:50:16 +0800

    now has binary file

---
+++ bin/instantwindemo.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'randprize'
+ @headstails={ "T"=> {'odds'=> 2,'name'=>'win tails','value'=>0},"H"=> {'odds'=> 2,'name'=>'win heads','value'=>1}}
+  @dice={}
+  1.upto(6) {|i| @dice[i.to_s]= {'odds'=> 6,'name'=>"rolled #{i}",'value'=>i} }
+   @pm=Randprize::ManagePrizes.new
+   @pm.prize_list(@dice)
+   1.upto(6) { |i|  puts @pm.random_prize}
